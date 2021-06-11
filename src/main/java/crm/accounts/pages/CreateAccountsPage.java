4369726ac07430680d524d32207701b7741ad24b
BG <mavie1322@yahoo.fr>
2021-06-11 16:37:45 +0100

    second commit

--- src/main/java/crm/accounts/pages/CreateAccountsPage.java
+++ src/main/java/crm/accounts/pages/CreateAccountsPage.java
@@ -8,9 +8,10 @@ import base.Page;
 
 public class CreateAccountsPage extends Page {
 
-	public void createAccount(String accountName, String accountSite) {
+	public void createAccount(String accountName, String accountSite) throws InterruptedException {
 		type("accName_CSS", accountName);
-		type("accName_CSS", accountSite);
+		Thread.sleep(1000);
+		type("accSite_CSS", accountSite);
 		//driver.findElement(By.cssSelector("[tabindex='7']")).sendKeys(accountName);
 	}
 	
